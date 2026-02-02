01b53f5778a3a7835c882975a0a788170cf9d4dd
Sh1ra0ri <xxnon.7@gmail.com>
2026-02-02 18:06:48 +0300

    Initial commit

--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from users.models import Payment
 from users.serializers import PaymentSerializer
-
+from rest_framework import generics
+from users.models import User
+from users.serializers import UserSerializer
 
 # Create your views here.
 class PaymentViewSet(viewsets.ModelViewSet):
@@ -13,3 +16,15 @@ class PaymentViewSet(viewsets.ModelViewSet):
     serializer_class = PaymentSerializer
     filterset_fields = ("paid_course", "paid_lesson", "payment_method")
     ordering_fields = ("date",)
+    permission_classes = (AllowAny,)
+
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
