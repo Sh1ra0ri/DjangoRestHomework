9a819e4d88979998d42fd77a41d988e6814ce029
Sh1ra0ri <xxnon.7@gmail.com>
2026-02-06 11:52:37 +0300

    .

--- materials/models.py
+++ materials/models.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.db import models
 
+User = settings.AUTH_USER_MODEL
 
 class Course(models.Model):
     title = models.CharField(
--- materials/tests.py
+++ materials/tests.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from users.models import User
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
