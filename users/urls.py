6504d6a7e421104749ce6e9e565306e439c4381f
Sh1ra0ri <xxnon.7@gmail.com>
2026-02-17 18:00:18 +0300

    .

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
 from users.apps import UsersConfig
-from users.views import PaymentViewSet, UserCreateAPIView
+from users.views import PaymentViewSet, UserCreateAPIView, StripePaymentCreateView
 
 app_name = UsersConfig.name
 router = SimpleRouter()
