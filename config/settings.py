0ec5d6b838cf9c4ddb6c7694091a73b31766d724
Sh1ra0ri  <xxnon.7@gmail.com>
2026-03-23 19:55:14 +0000

    .

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.242.84', 'localhost', '127.0.0.1']
 
 # Application definition
 
